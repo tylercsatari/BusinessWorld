cde0f2df3695ae6f65eb9803dfcfaca53f6d5afa
Tyler Csatari <tylerdaviscsatari@gmail.com>
2026-04-15 12:30:11 -0600

    Sync Jarvis state: run auto_20260415180810 active; 1231 graph nodes; Group T metrics (visual-credibility/payoff-signal/proof-arrival/delayed-reveal) in progress; 129 completed this run; add verify-r2.js + gitignore full_backup

---
+++ buildings/jarvis/verify-r2.js
@@ -0,0 +1,16 @@
+require('dotenv').config({ path: require('path').join(__dirname, '../../.env') });
+const cs = require('../../cloud-storage');
+cs.initR2();
+async function check() {
+  const buf = await cs.downloadFromR2('jarvis/derived_experiments.json');
+  const d = JSON.parse(buf.toString());
+  const arr = d.experiments || d;
+  console.log('R2 derived_experiments:', Array.isArray(arr) ? arr.length : Object.keys(arr).length);
+  const buf2 = await cs.downloadFromR2('jarvis/graph.json');
+  const d2 = JSON.parse(buf2.toString());
+  console.log('R2 graph:', (d2.nodes||[]).length, 'nodes');
+  const buf3 = await cs.downloadFromR2('jarvis/autonomous_progress.json');
+  const d3 = JSON.parse(buf3.toString());
+  console.log('R2 progress: active=' + d3.active + ' completed=' + d3.completed + ' run=' + d3.run_id);
+}
+check().catch(e=>console.error(e.message));
